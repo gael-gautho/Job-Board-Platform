cb5fd9de8290c957a736c5196b50cc61fc8401f2
Gael Gautho <gaelgautho07@gmail.com>
2025-08-07 08:36:45 +0200

    Init backend ,account app and signup

---
+++ django_backend/account/api.py
@@ -0,0 +1,33 @@
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from .forms import SignupForm
+
+
+@api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
+def signup(request):
+    data = request.data
+    message = 'success'
+
+    form = SignupForm({
+        'email': data.get('email'),
+        'name': data.get('name'),
+        'password1': data.get('password1'),
+        'password2': data.get('password2'),
+    })
+
+    if form.is_valid():
+        user = form.save()
+        user.save()
+        return JsonResponse(
+            {"message": "success"},
+            status=status.HTTP_201_CREATED, safe=False  
+        )
+
+    else:
+        message = form.errors
+        return JsonResponse( message,
+                            status=status.HTTP_400_BAD_REQUEST, safe=False)
+
