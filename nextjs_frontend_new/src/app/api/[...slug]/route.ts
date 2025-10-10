f7d49d2815a065011b4fe7a0d45ad0d889aeb390
Gael Gautho <gaelgautho07@gmail.com>
2025-10-10 10:24:42 +0200

    Fix application form

--- nextjs_frontend_new/src/app/api/[...slug]/route.ts
+++ nextjs_frontend_new/src/app/api/[...slug]/route.ts
@@ -1,15 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
-async function handleProxy(req: NextRequest, context: { params: { slug: string[] } }) {
-  const { params } = context;
-  //const path = (await params).slug.join("/");
+async function handleProxy(req: NextRequest, context: { params: Promise<{ slug: string[] }> }) {
   
   const path = req.nextUrl.pathname.replace('/api', '');
   const url = `${process.env.NEXT_PUBLIC_API_HOST}/${path}/`;
   console.log(req.cookies.get('session_access_token'))
-
-  const bd = await req.text()
-  console.log(bd)
 
   const res = await fetch(url, {
     method: req.method,
@@ -17,7 +12,7 @@ async function handleProxy(req: NextRequest, context: { params: { slug: string[]
       "Content-Type": req.headers.get("Content-Type") || "application/json",
       "Authorization": `Bearer ${req.cookies.get('session_access_token')?.value}` || "",
     },
-    body: req.method === "GET" || req.method === "HEAD" ? undefined : bd, //await req.text(),
+    body: req.method === "GET" || req.method === "HEAD" ? undefined : await req.text(),
   });
 
   const data = await res.json();
@@ -25,18 +20,22 @@ async function handleProxy(req: NextRequest, context: { params: { slug: string[]
 
 }
 
-export async function GET(req: NextRequest, context: { params: { slug: string[] } }) {
+export async function GET(req: NextRequest, context: { params: Promise<{ slug: string[] }> }) {
+
   return handleProxy(req, context);
 }
 
-export async function POST(req: NextRequest, context: { params: { slug: string[] } }) {
+export async function POST(req: NextRequest, context: { params: Promise<{ slug: string[] }> }) {
+
   return handleProxy(req, context);
 }
 
-export async function PUT(req: NextRequest, context: { params: { slug: string[] } }) {
+export async function PUT(req: NextRequest, context: { params: Promise<{ slug: string[] }> }) {
+
   return handleProxy(req, context);
 }
 
-export async function DELETE(req: NextRequest, context: { params: { slug: string[] } }) {
+export async function DELETE(req: NextRequest, context: { params: Promise<{ slug: string[] }> }) {
+
   return handleProxy(req, context);
 }
